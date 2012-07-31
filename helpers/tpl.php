ee184b0fb5dd144331e25a1d2abd57343dc16ab4
Baptiste Fontaine <batifon@yahoo.fr>
2012-07-31 14:25:21 +0200

    tpl_array() fixed

--- helpers/tpl.php
+++ helpers/tpl.php
@@ -2,6 +2,28 @@
 
 # Templates helpers
 
+# version of array_merge_recursive without overwriting numeric keys
+# picked from
+# http://www.php.net/manual/fr/function.array-merge-recursive.php#106985
+function array_merge_recursive_new() {
+
+    $arrays = func_get_args();
+    $base = array_shift($arrays);
+
+    foreach ($arrays as $array) {
+        reset($base); //important
+        while (list($key, $value) = @each($array)) {
+            if (is_array($value) && @is_array($base[$key])) {
+                $base[$key] = array_merge_recursive_new($base[$key], $value);
+            } else {
+                $base[$key] = $value;
+            }
+        }
+    }
+
+    return $base;
+}
+
 # Merge default templates values with the given
 # array, and return a new array.
 function tpl_array($arr) {
@@ -9,7 +31,7 @@ function tpl_array($arr) {
     if (empty($arr)) {
         return Config::$default_tpl_values;
     }
-    return array_merge(Config::$default_tpl_values, $arr);
+    return array_merge_recursive_new(Config::$default_tpl_values, $arr);
 }
 
 ?>
