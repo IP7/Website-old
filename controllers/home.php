ad3010f53826ff670f123f8862eb6a4f8ed3f28e
Baptiste Fontaine <batifon@yahoo.fr>
2012-07-31 12:22:19 +0200

    Using home.html template.

--- controllers/home.php
+++ controllers/home.php
@@ -1,27 +1,15 @@
 <?php
 
-// temporary solution
 function display_home() {
 
-    return <<<EOF
-<!doctype html>
-<html lang="fr" dir="ltr">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="language" content="fr" />
-        <title>IP7</title>
+    return Config::$tpl->render('home.html', tpl_array(array(
+        'page' => array(
+            'title' => 'IP7 – Accueil'
+        ),
 
-        <meta name="keywords" content="ip7, infop7, diderot, informatique, paris" />
-        <meta name="description" content=""/>
-
-        <meta name="viewport" content="width=device-width" />
-    </head>
-        <body>
-            <p>Bienvenue sur le site de la future association IP7.</p>
-        </body>
-</html>
-EOF
-;
+        // temporary solution
+        'welcome' => 'Bienvenue sur le site de la future association IP7.'
+    )));
 
 }
 
