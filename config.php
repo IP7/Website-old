e3391798a8dc9268667a42639adf16d1843e7203
Baptiste Fontaine <batifon@yahoo.fr>
2012-08-02 21:48:07 +0200

    deactived_account constant renamed

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ require_once dirname(__FILE__).'/lib/vendors/propel/runtime/lib/Propel.php';
 # - login constants
 #
 define('WRONG_USERNAME_OR_PASSWORD' -2);
-define('ACCOUNT_DEACTIVED' -3);
+define('DEACTIVED_ACCOUNT' -3);
 #
 #
 # - access rights constants
