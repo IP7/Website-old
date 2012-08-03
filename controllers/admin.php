159984a30faa17313cf23ba3d5a4d1084996c779
Baptiste Fontaine <batifon@yahoo.fr>
2012-08-03 23:10:05 +0200

    Admin test controllers added

--- controllers/admin.php
+++ controllers/admin.php
@@ -1,9 +1,16 @@
 <?php
 
-require '../config.php';
+require_once dirname(__FILE__).'/../config.php';
 Config::init();
 
-function display admin_home() {
+require_once dirname(__FILE__).'/../helpers/login.php';
+
+function display_admin_home() {
+
+    if (!is_connected()) {
+        return 'test:admin_home not connected.'; # TODO
+    }
+
     return "test:admin_home"; # TODO
 }
 
