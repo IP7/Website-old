543f4305d89259961202e0429a7fa12803d87b32
Baptiste Fontaine <batifon@yahoo.fr>
2012-08-04 17:23:26 +0200

    Home test page added

--- controllers/home.php
+++ controllers/home.php
@@ -1,10 +1,44 @@
 <?php
 
+require_once dirname(__FILE__).'/../config.php';
+Config::init();
+
 function display_home() {
 
-    return Config::$tpl->render('home.html', tpl_array(array(
+    $message = false;
+    $message_type = '';
+
+    # Connection
+    if (   (isset($_POST['username']) && !empty($_POST['username']))
+        && (isset($_POST['password']) && !empty($_POST['password']))) {
+
+            # sanitization made by Propel
+            $res = connection(
+                $_POST['username'],
+                $_POST['password'],
+                (isset($_POST['remember']) ? (bool)$_POST['remember'] : false)
+            );
+
+            if ($res !== CONNECTION_OK) {
+                $message_type = 'error';
+                $message = error_message($res);
+            }
+    }
+    # Disconnection
+    else if (isset($_GET['disconnect'])) {
+        disconnection();
+    }
+
+    # Rendering
+    return Config::$tpl->render('_test_home.html', tpl_array(array(
         'page' => array(
-            'title' => 'Accueil'
+            'title' => 'Accueil',
+
+            'message' => $message,
+            'message_type' => $message_type,
+
+            'show_connection_form' => !is_connected(),
+            'show_disconnection_link' => is_connected()
         ),
 
         // temporary solution
