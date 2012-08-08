0605995a42bcbb0454bf9b3d836fdfd3668afe14
Baptiste Fontaine <batifon@yahoo.fr>
2012-08-08 22:47:23 +0200

    setting global links in function of user's state

--- helpers/tpl.php
+++ helpers/tpl.php
@@ -27,10 +27,40 @@ function array_merge_recursive_new() {
     return $base;
 }
 
+# Returns an array for site.global_links.others in function of user's state,
+# i.e. connected or not
+function global_menu_links() {
+
+    if (!is_connected()) {
+        return array(
+            'site' => array(
+                'global_links' => array(
+                    'others' => array(
+                        array( 'href' => Config::$root_uri.'connexion', 'title' => 'Connexion' )
+                    )
+                )
+            )
+        );
+    }
+
+    return array(
+        'site' => array(
+            'global_links' => array(
+                'others' => array(
+                    array( 'href' => Config::$root_uri.'forum',                   'title' => 'Forum' ),
+                    array( 'href' => Config::$root_uri.'~'.user()->getUsername(), 'title' => 'Mon Profil'),
+                    array( 'href' => Config::$root_uri.'?disconnect',             'title' => 'Déconnexion')
+                )
+            )
+        )
+    );
+}
+
 # Merge default templates values with the given
 # array, and return a new array.
 function tpl_array() {
     $arrays = func_get_args();
+    array_unshift(&$arrays, global_menu_links());
     array_unshift(&$arrays, Config::$default_tpl_values);
     return call_user_func_array('array_merge_recursive_new', $arrays);
 }
