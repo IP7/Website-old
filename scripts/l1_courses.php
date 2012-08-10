b0f1e12c67ba3f32f931f481b9f5b0dc63deae84
Baptiste Fontaine <batifon@yahoo.fr>
2012-08-10 16:13:38 +0200

    L1 courses updated (cf #38)

--- scripts/l1_courses.php
+++ scripts/l1_courses.php
@@ -11,17 +11,21 @@ $c = array(
             'IS1'      => array(3, 'Initiation aux systèmes d\'exploitation.'),
             'PF1'      => array(3, 'Principe de fonctionnement des machines binaires.'),
             'OBi'      => array(3, 'Outils bureautique et internet : matière commune à tous les étudiants.'),
-            'MI1'      => array(9, 'Mathématiques.')
+            'MM1'      => array(9, 'Mathématiques élémentaires.')
         ),
 
         // facultatif (UE libres)
         array(
-            'LM1' => array(3, 'Langage mathématique.'),
-            'SD1' => array(3, 'Statistiques descriptives.'),
             'BC1' => array(6, 'Biologie cellulaire et moléculaire expérimentale.'),
             'CH1' => array(6, 'Chimie : Atomes et molécules.'),
             'AR1' => array(3, 'Géosciences : Actualité de la recherche.'),
-            'PA1' => array(3, 'Géosciences : Panorama 1')
+            'TB1' => array(3, 'La Terre est bleue comme une orange.'),
+            'PA1' => array(3, 'Géosciences : Panorama 1'),
+            'LM1' => array(3, 'Langage mathématique.'),
+            'SD1' => array(3, 'Statistiques descriptives.'),
+            'PH1' => array(6, 'Physique.'),
+            'OP1' => array(3, 'Physique de la lumière.'),
+            'DV1' => array(6, 'Diversité et évolution des organismes vivants.')
         )
     ),
 
@@ -33,8 +37,9 @@ $c = array(
             'TO2'     => array(6, 'Types de données et objets.'),
             'CI2'     => array(3, 'Concepts Informatique.'),
             'IO2'     => array(6, 'Internet et outils : Conception d\'un site Web avec HTML, CSS, PHP et MySQL.'),
-            'MI2'     => array(9, 'Mathématiques.'),
-            'Anglais' => array(3, 'Autoformation à l\'anglais.')
+            'MI2'     => array(9, 'Mathématiques générales.'),
+            'Anglais' => array(3, 'Autoformation à l\'anglais.'),
+            'UE libre' => array(3, 'Enseignement libre au choix ou stage.')
         ),
 
         array()
