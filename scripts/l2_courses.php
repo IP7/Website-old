83ab398cfecd8612668bb6555ddf92f35cba687e
Baptiste Fontaine <batifon@yahoo.fr>
2012-08-10 12:25:57 +0200

    fixes #33.

---
+++ scripts/l2_courses.php
@@ -0,0 +1,43 @@
+<?php
+
+$c = array(
+
+    // S1
+    array(
+
+        // obligatoire
+        array(
+            'OL3'     => array(6, 'Outils logiques.'),
+            'PI3'     => array(6, 'Projet de programmation.'),
+            'EC3'     => array(3, 'Éléments de combinatoire.'),
+            'MI3'     => array(9, 'Mathématiques.'),
+            'Anglais' => array(3, ''),
+            'PPP'     => array(3, 'Projet professionel.')
+        ),
+
+        // facultatif (UE libres)
+        array(
+        )
+    ),
+
+    // S2
+    array(
+
+        // obligatoire
+        array(
+            'LC4'      => array(6, 'Langage C.'),
+            'AF4'      => array(6, 'Automates finis.'),
+            'EA4'      => array(3, 'Éléments d\'algorithmique.'),
+            'LS4'      => array(3, 'Langage de scripts : Python.'),
+            'EP4'      => array(3, 'Éléments de probabilités.'),
+            'MI4'      => array(6, 'Mathématiques.'),
+            'UE libre' => array(3, 'Enseignement d\'une autre mention au choix ou stage.')
+        ),
+
+        array()
+    )
+
+);
+
+?>
+
