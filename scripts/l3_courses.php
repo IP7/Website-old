f6d54807e1a31be31eed678da95e19113460c18a
Baptiste Fontaine <batifon@yahoo.fr>
2012-08-10 12:56:35 +0200

    L3 courses added (not finished), cf #34

---
+++ scripts/l3_courses.php
@@ -0,0 +1,43 @@
+<?php
+
+$c = array(
+
+    // S1
+    array(
+
+        // obligatoire
+        array(
+            'PF5' => array(6, 'Initiation à la programation fonctionnelle avec OCaml.'),
+            'PO5' => array(6, 'Programmation orientée objets avec Java.'),
+            'MD5' => array(6, 'Mathématiques discrètes : séries génératrices et automates à piles.'),
+            'SY5' => array(6, 'Programmation système en C.'),
+            'AL5' => array(6, 'Algorithmique.')
+        ),
+
+        // facultatif (UE libres)
+        array(
+        )
+    ),
+
+    // S2
+    array(
+
+        // obligatoire
+        array(
+            'LO6' => array(5, 'Logique.'),
+            'BD6' => array(5, 'Base de données : PostgreSQL.'),
+            'PR6' => array(5, 'Programmation réseau en Java.'),
+            'AS6' => array(5, 'Analyse Syntaxique.')
+        ),
+
+        array(
+            'Stage' => array(7, 'Stage long.'), // TODO trouver le code de la matière
+            'Probabilités' => array(7, 'Probabilités') // TODO trouver le code de la matière
+            // TODO
+        )
+    )
+
+);
+
+?>
+
