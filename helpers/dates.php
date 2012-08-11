262f0de8d10c92b066359393a20157489fcdf778
Baptiste Fontaine <batifon@yahoo.fr>
2012-08-11 18:08:24 +0200

    date_fr fixed

--- helpers/dates.php
+++ helpers/dates.php
@@ -3,7 +3,13 @@
 # format a date for French output
 function date_fr($d) {
     if (is_string($d)) {
+        try {
         $d = new DateTime($d);
+        }
+        catch (Exception $e) {
+            error_log($e);
+            $d = new DateTime(substr($d, 1, -1));
+        }
     }
 
     $str = $d->format('d/m/Y'); 
