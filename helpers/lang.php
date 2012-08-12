6f1e648d5dd554215dfff9a514b279b7064f1b64
Baptiste Fontaine <batifon@yahoo.fr>
2012-08-12 20:07:20 +0200

    adapt_to_gender function added

---
+++ helpers/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+/* adapt a string to the gender of the user
+ * if $gender is 'F', $female_suffix is appended, $male_suffix if not
+ *
+ * e.g.: adapt_to_gender($u, 'etudiant') -> 'etudiante' // female
+ *       adapt_to_gender($u, 'etudiant') -> 'etudiant'  // male
+ */
+function adapt_to_gender($user, $s, $female_suffix='e', $male_suffix='') {
+    return $s . (($user->getGender() == 'F') ? $female_suffix : $male_suffix;
+}
+
+?>
+
