f5ab90fce38640cbd1aa578a293d73a74e4b6d41
Baptiste Fontaine <batifon@yahoo.fr>
2012-08-20 00:37:54 +0200

    get_random_password helper added

--- helpers/login.php
+++ helpers/login.php
@@ -1,142 +1,155 @@
 <?php
 
-	require_once 'config.php';
-	Config::init();
+require_once 'config.php';
+Config::init();
 
-    /**
-     * returns $_SESSION['user']
-     **/
-    function user() {
-        return $_SESSION['user'];
+/**
+ * returns $_SESSION['user']
+ **/
+function user() {
+    return $_SESSION['user'];
+}
+
+/**
+ * Verify if user's credentials are valid. If so,
+ * return the corresping User object. If the credentials
+ * are invalid, return DEACTIVATED_ACCOUNT or
+ * WRONG_USERNAME_OR_PASSWORD.
+ *
+ * @param	String $username	username
+ * @param	String $password	password
+ **/
+function check_user_credentials($username, $password){
+
+    $username = (string)$username;
+    $password = (string)$password;
+
+    $user = UserQuery::create()
+                ->findOneByUsername($username);
+
+    if ( $user instanceof User ){
+        if ( Config::$p_hasher->CheckPassword($password, $user->getPasswordHash()) ){
+            if ( $user->isActivated() ){
+                return $user;
+            }
+            return DEACTIVATED_ACCOUNT;
+        }
+    }
+    return WRONG_USERNAME_OR_PASSWORD;
+}
+
+
+/**
+ *
+ * Connect and return an user.
+ *
+ * @param	String $username	username
+ * @param	String $password	password
+ * @see check_user_credentials
+ **/
+function connection($username, $password, $remember=false) {
+
+    if (is_connected()) {
+        return ALREADY_CONNECTED;
     }
 
-	/**
-     * Verify if user's credentials are valid. If so,
-     * return the corresping User object. If the credentials
-     * are invalid, return DEACTIVATED_ACCOUNT or
-     * WRONG_USERNAME_OR_PASSWORD.
-	 *
-	 * @param	String $username	username
-	 * @param	String $password	password
-	 **/
-	function check_user_credentials($username, $password){
-	
-		$username = (string)$username;
-		$password = (string)$password;
+    $user = check_user_credentials($username, $password);
 
-		$user = UserQuery::create()
-                    ->findOneByUsername($username);
-
-		if ( $user instanceof User ){
-			if ( Config::$p_hasher->CheckPassword($password, $user->getPasswordHash()) ){
-				if ( $user->isActivated() ){
-					return $user;
-				}
-				return DEACTIVATED_ACCOUNT;
-			}
-		}
-		return WRONG_USERNAME_OR_PASSWORD;
-	}
-
-
-    /**
-     *
-     * Connect and return an user.
-	 *
-	 * @param	String $username	username
-	 * @param	String $password	password
-     * @see check_user_credentials
-	 **/
-    function connection($username, $password, $remember=false) {
-
-        if (is_connected()) {
-            return ALREADY_CONNECTED;
-        }
-
-        $user = check_user_credentials($username, $password);
-
-        if (!( $user instanceof User )) {
-            return $user; // error code
-        }
-
-        return set_connected($user, $remember);
+    if (!( $user instanceof User )) {
+        return $user; // error code
     }
 
+    return set_connected($user, $remember);
+}
 
-    /**
-     * Set $_SESSION & $_COOKIE variables to connect the given user.
-     * Returns CONNECTION_OK.
-     **/
-    function set_connected($user, $remember=false) {
-        $_SESSION['user'] = $user;
-        if ($remember) {
-            setcookie(AUTH_COOKIE,
-                      $user->getUsername().':'.$user->getPasswordHash(),
-                      AUTH_COOKIE_EXPIRE,
-                      '/');
-                      # production: use the line below.
-                      # 'wwww.infop7.org');
-        }
 
-        if (!isset($_SESSION['visit_counted'])) {
-            $user->incrementVisitsNb();
-            $user->setLastVisit(new DateTime());
-            $_SESSION['visit_counted'] = true;
-        }
-
-        return CONNECTION_OK;
+/**
+ * Set $_SESSION & $_COOKIE variables to connect the given user.
+ * Returns CONNECTION_OK.
+ **/
+function set_connected($user, $remember=false) {
+    $_SESSION['user'] = $user;
+    if ($remember) {
+        setcookie(AUTH_COOKIE,
+                  $user->getUsername().':'.$user->getPasswordHash(),
+                  AUTH_COOKIE_EXPIRE,
+                  '/');
+                  # production: use the line below.
+                  # 'wwww.infop7.org');
     }
 
-    /**
-     * Disconnect the current connected user. Return false if no one
-     * is connected.
-     **/
-    function disconnection() {
-        if (!is_connected()) { return false; }
-        setcookie(AUTH_COOKIE); // remove cookie
-        unset($_SESSION['user']);
-        session_unset();
-        session_destroy();
-        return true;
+    if (!isset($_SESSION['visit_counted'])) {
+        $user->incrementVisitsNb();
+        $user->setLastVisit(new DateTime());
+        $_SESSION['visit_counted'] = true;
     }
 
+    return CONNECTION_OK;
+}
 
-    /**
-     * Test if an user is already connected, and return true or false.
-     **/
-    function is_connected() {
-        return (isset($_SESSION['user']) && ($_SESSION['user'] instanceof User));
+/**
+ * Disconnect the current connected user. Return false if no one
+ * is connected.
+ **/
+function disconnection() {
+    if (!is_connected()) { return false; }
+    setcookie(AUTH_COOKIE); // remove cookie
+    unset($_SESSION['user']);
+    session_unset();
+    session_destroy();
+    return true;
+}
+
+
+/**
+ * Test if an user is already connected, and return true or false.
+ **/
+function is_connected() {
+    return (isset($_SESSION['user']) && ($_SESSION['user'] instanceof User));
+}
+
+/**
+ * Try to connect automatically an user if a cookie is set
+ **/
+function try_autoconnect() {
+    if (is_connected()) {
+        return ALREADY_CONNECTED;
     }
 
-    /**
-     * Try to connect automatically an user if a cookie is set
-     **/
-    function try_autoconnect() {
-        if (is_connected()) {
-            return ALREADY_CONNECTED;
-        }
-
-        if (!isset($_COOKIE) || !isset($_COOKIE[AUTH_COOKIE])) {
-            return false;
-        }
-
-        $credentials = explode(':', $_COOKIE[AUTH_COOKIE]);
-
-        if (count($credentials) < 2) { return false; }
-
-        $username = $credentials[0];
-        $hash     = $credentials[1];
-
-        $user = UserQuery::create()
-                    ->filterByUsername($username)
-                    ->filterByPasswordHash($hash)
-                    ->findOne();
-
-        if ($user instanceof User) {
-            return set_connected($user, true);
-        }
-
+    if (!isset($_COOKIE) || !isset($_COOKIE[AUTH_COOKIE])) {
         return false;
     }
+
+    $credentials = explode(':', $_COOKIE[AUTH_COOKIE]);
+
+    if (count($credentials) < 2) { return false; }
+
+    $username = $credentials[0];
+    $hash     = $credentials[1];
+
+    $user = UserQuery::create()
+                ->filterByUsername($username)
+                ->filterByPasswordHash($hash)
+                ->findOne();
+
+    if ($user instanceof User) {
+        return set_connected($user, true);
+    }
+
+    return false;
+}
+
+// generate a random password
+function get_random_password($length=10) {
+    // without 'l' and 'I', and 'o', 'O', '0' to avoid confusion
+    $c = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz123456789";
+    $c_len = strlen($c)-1;
+    $s = '';
+
+    for($i=0; $i < $length; $i++) {
+        $s .= $c[rand(0, $c_len)];
+    }
+    return str_shuffle($s);
+}
 
 ?>
