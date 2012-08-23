a501c2bc25c1c031c490a9f66913fb1dc13b5f69
Baptiste Fontaine <batifon@yahoo.fr>
2012-08-23 17:58:15 +0200

    migrate.sh now check if there is existant migration files, and delete them when the .sql file has been generated

--- models/migrate.sh
+++ models/migrate.sh
@@ -8,22 +8,38 @@
 # 'migration.sql' file if it succeeds.
 #
 
+function _migrate() {
 
 if [ $# -lt 2 ]; then
-    f=$(\ls -1 ./build/migrations/PropelMigration_* | tail -1)
-    f=${f##*/}
+    files=$(\ls -1 ./build/migrations/PropelMigration_*php 2> /dev/null);
+    if [ $? -ne 0 ]; then
+        echo "error, no migration file found."
+        return 1
+    fi
+    f=${files##*/}
 else
     f="PropelMigration_$1.php"
 fi
 
 echo "Using '$f'."
 
+# extract SQL migration code
 php --run "
 include './build/migrations/$f';
 \$p = new ${f%%.php}();
 \$u = \$p->getUpSQL();
 file_put_contents('${f%%php}sql', \$u['infop7db']);"
+
+# Add the DB name
 echo "USE infop7db;" > migration.sql
 cat ${f%%php}sql >> migration.sql
 rm ${f%%php}sql
+
+# remove the migration PHP file (keep a backup)
+mv ./build/migrations/$f{,.done}
+
 echo "SQL migration code is in 'migration.sql'."
+
+}
+
+_migrate
