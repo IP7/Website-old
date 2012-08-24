d7eb5dfe46b180edbebc33c48b9129c884ecb62b
Aaylor <loloruna@orange.fr>
2012-08-24 18:13:40 +0200

    add message function

---
+++ helpers/message.php
@@ -0,0 +1,30 @@
+<?php
+
+	require_once 'config.php';
+	Config::init();
+
+	function set_message($string){
+
+		$string = (string)$string;
+		$random = rand();
+
+		$_SESSION['message'][$random] = $string;
+
+		return $random;
+
+	}
+
+	function get_message($idMessage){
+
+		if ( isset($_SESSION['message'][$idMessage]) && !empty($_SESSION['message'][$idMessage]) ){
+			
+			$msg = $_SESSION['message'][$idMessage];
+			unset($_SESSION['message'][$idMessage]);
+
+			return $msg;
+	
+		}
+
+	}
+
+?>
