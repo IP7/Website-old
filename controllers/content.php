05f0422de1ebb03a20cff56f315c39ade11749ca
Aaylor <loloruna@orange.fr>
2012-08-30 16:18:42 +0200

    function to display content

---
+++ controllers/content.php
@@ -0,0 +1,58 @@
+<?php
+
+function display_content_view(){
+
+	$contentId = (int)params('id');
+	$cursusSN = (string)params('cursus');
+	$courseCode = (string)params('course');
+
+	$content	= ContentQuery::create()->findOneById($contentId);
+	$user		= $content->getAuthor();
+	$course	= $content->getCourse();
+	$cursus	= $content->getCursus();
+
+	$report = ReportQuery::create()->findOneByContentId($contentId);
+	$reportArray = Array();
+
+	if ( (strtoupper($cursusSN) != $cursus->getShortName()) && (strtoupper($courseCode) != $course->getCode()) )
+		halt(NOT_FOUND);
+
+	if ( user()->isAdmin() && $report instanceof Report ){
+
+		$option = Array(
+				Array(	'title' => 'Valider'),
+				Array(	'title' => 'Supprimer')
+			);
+	
+		$reportArray = Array(
+				'reportId' => $report->getId(),
+				'contentId' => $contentId,
+				'author' => $report->getAuthor()->getUsername(),
+				'reportDate' => $report->getDate(),
+				'reason' => $report->getText(),
+				'href' => Config::$root_uri . 'admin/reports',
+				'options' => $option
+			);
+
+	}
+
+	$contentArray = Array(
+				'title' => $content->getTitle(),
+				'cursus' => $cursus->getName(),
+				'contentText' => $content->getText(),
+				'courseName' => $course->getName(),
+				'courseCode' => $course->getCode(),
+				'date' => $content->getDate(),
+				'username' => $user->getUsername()
+			);
+
+	return Config::$tpl->render('content_view.html', tpl_array(admin_tpl_default(),array(
+					'page' => Array(
+						'report' => $reportArray,
+						'content' => $contentArray
+					)
+		)));
+
+}
+
+?>
