908972c1165233cb2eb4f27445ec49436b06b7f2
Baptiste Fontaine <batifon@yahoo.fr>
2012-09-04 14:42:57 +0200

    has_file() helper added

--- helpers/requests.php
+++ helpers/requests.php
@@ -10,4 +10,9 @@ function has_get($name, $allow_empty=false) {
     return (isset($_GET[$name]) && (!empty($_GET[$name]) || $allow_empty == empty($_GET[$name])));
 }
 
+// check if $_FILES[$name] exists and is not empty
+function has_file($name, $allow_empty=false) {
+    return (isset($_FILES[$name]) && (!empty($_FILES[$name]) || $allow_empty == empty($_FILES[$name])));
+}
+
 ?>
