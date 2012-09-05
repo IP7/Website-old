2b8eb99727895038a05b5d7c99523ff6ab5a7bea
Baptiste Fontaine <batifon@yahoo.fr>
2012-09-05 13:40:07 +0200

    cursus_url() helper added

--- helpers/urls.php
+++ helpers/urls.php
@@ -5,9 +5,15 @@ function url() {
   return $_SERVER['REQUEST_URI'];
 }
 
+// return the current URL without the Config::$root_uri prefix
 function config_url() {
     $u = url();
     return str_ireplace(Config::$root_uri, '', $u);
+}
+
+// return the URL of a cursus
+function cursus_url($c) {
+    return Config::$root_uri.'cursus/'.$c->getShortName();
 }
 
 ?>
