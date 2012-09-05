56790f9c52bd3ab14a573c6db632496bd810b7fb
Baptiste Fontaine <batifon@yahoo.fr>
2012-09-05 15:48:51 +0200

    display_proposing_content_form() controller added

--- controllers/content.php
+++ controllers/content.php
@@ -55,4 +55,55 @@ function display_content_view(){
 
 }
 
+function display_proposing_content_form() {
+
+    $cursus_name = params('cursus');
+    $course_name = params('course');
+
+    $cursus = CursusQuery::create()->findOneByShortName($cursus_name);
+
+    if (!$cursus) {
+        halt(NOT_FOUND);
+    }
+
+    $course = CourseQuery::create()
+                            ->filterByCursus($cursus)
+                            ->findOneByCode($course_name);
+
+    if (!$course) {
+        halt(NOT_FOUND);
+    }
+
+    if (!is_connected() || !user()->isMember()) {
+        halt(HTTP_FORBIDDEN, "L'accès à cette page est réservé aux membres.");
+    }
+
+    $url = course_url($cursus, $course).'/proposer';
+
+    return tpl_render('contents/proposing.html', array(
+        'page' => array(
+            'title' => 'Proposer un contenu '.de($course->getCode()),
+
+            'breadcrumbs' => array(
+                1 => array(
+                    'href'  => cursus_url($cursus),
+                    'title' => $cursus->getName()
+                ),
+                2 => array(
+                    'href'  => course_url($cursus,$course),
+                    'title' => $course->getCode()
+                ),
+                3 => array(
+                    'href'  => $url,
+                    'title' => 'Proposer un contenu'
+                )
+            ),
+
+            'form' => array(
+                'action' => $url
+            )
+        )
+    ));
+}
+
 ?>
