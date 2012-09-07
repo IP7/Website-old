19f9aca4b2de3c0cf6aea2703c1600299fb883c7
Baptiste Fontaine <batifon@yahoo.fr>
2012-09-07 13:09:16 +0200

    fixes #138.

--- views/static/js/tabs.js
+++ views/static/js/tabs.js
@@ -2,13 +2,13 @@ $(function(){
     (function($ta,s,$w,e) {
         // add a .selected class on the selected tab
         $w.bind(e, function(h){
-            h=window.location.hash;
+            if(!(h=window.location.hash))return;
             $ta.parent().removeClass(s);
             $('.tab a[href='+h+']').parent().addClass(s);
         });
 
         // automatically open the first tab if no tab is selected
-        if(!window.location.hash)window.location.hash=$ta.first().attr('href');
+        if(!window.location.hash&&$ta.length)window.location.hash=$ta.first().attr('href');
         // trigger hashchange on pageload
         $w.trigger(e);
     })($('.tab a'), 'selected', $(window), 'hashchange');
