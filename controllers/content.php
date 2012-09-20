247446d3a4126fccc84610d34a3186d75f796afa
Baptiste Fontaine <batifon@yahoo.fr>
2012-09-20 15:17:14 +0200

    fix #175.

--- controllers/content.php
+++ controllers/content.php
@@ -233,7 +233,7 @@ function display_member_proposing_content_form() {
             ),
 
             'scripts' => array(
-                array( 'src' => js_url('proposing-content') )
+                array( 'href' => js_url('proposing-content') )
             )
         )
     ));
