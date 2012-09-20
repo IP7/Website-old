76181cbcf3a7404d2ba9277ea2400973d4dc36b1
Baptiste Fontaine <batifon@yahoo.fr>
2012-09-20 16:51:29 +0200

    using minified version of Mousetrap

--- config.php
+++ config.php
@@ -108,7 +108,7 @@ class Config {
                 ),
                 'scripts'           => array(
                     array( 'href' => $scripts.'/jquery-ui-1.8.23.custom.js' ),
-                    array( 'href' => $scripts.'/mousetrap.js'),
+                    array( 'href' => $scripts.'/mousetrap.min.js'),
                     array( 'href' => $scripts.'/search.min.js' ),
                     array( 'href' => $scripts.'/shortcuts.min.js' )
                 ),
