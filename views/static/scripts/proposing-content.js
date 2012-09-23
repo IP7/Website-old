45159347456180d9cf95d85b706a7711c96102aa
Baptiste Fontaine <batifon@yahoo.fr>
2012-09-23 22:53:59 +0200

    the 'type' field’s value is now automatically guessed when a title is provided

--- views/static/scripts/proposing-content.js
+++ views/static/scripts/proposing-content.js
@@ -4,6 +4,9 @@ $(function(){
         f  = d.getElementsByClassName('files_inputs')[0],
         maxf = f.dataset['max'],
 
+        title_inp = d.querySelector('input[name=title]'),
+        type_inp  = d.querySelector('select[name=type]'),
+
         inps = [],
 
         addInp = function() {
@@ -17,11 +20,14 @@ $(function(){
             i.type = 'file';
             i.name = 'userfiles[]';
             i.accept = 'text/*,application/*';
+
+            // display/hide microcopy
             i.onchange = function(){
                 this.parentElement
                         .children[1]
                             .classList[this.value?'add':'remove']('hidden');
-                addInp();
+                if (this.value)
+                    addInp();
             };
             p.className = 'microcopy';
             p.innerText = 'Facultatif, 10Mio max.';
@@ -45,6 +51,62 @@ $(function(){
             f.appendChild(l);
             f.appendChild(k);
         };
+
+        if (title_inp && type_inp) {
+            
+            // for each type, an array of its words
+            var types = [].slice.apply(type_inp.options)
+                            .map(function(o){
+                                return o.innerText
+                                            .replace(/^\s+|\s+$/g,'') // strip
+                                            .toLocaleLowerCase()
+                                            .split(/\s+/);
+                            }),
+                tl = types.length;
+
+            // update the 'type' input accordingly to 'title' input’s value
+
+            title_inp.onblur = function(/*placeholder:*/words, i, wl, index, max) {
+                if (+type_inp.value > 0) { return; }
+
+                // words of the title
+                title_words = title_inp.value.toLocaleLowerCase().split(/\s+/);
+                var wl = title_words.length,
+                max = 0,
+                j, type, tll, local_max;
+
+                for (i=0; i<tl; i++) {
+                    // for each type, we count the number of words which are in
+                    // the title. 'max' is the maximum number found until now,
+                    // 'index' the index of the type which has this 'max' number of
+                    // matching words.
+                    type=types[i], tll=type.length, local_max=0;
+
+                    for (j=0; j<tll; j++) {
+                        if (title_words.indexOf(type[j]) > -1) { local_max++; }
+                    }
+
+                    if (local_max > max) {
+                        max = local_max;
+                        index = i;
+                    }
+                }
+
+                type_inp.value = index;
+                title_inp.onblur = null;
+
+                /*
+                for (i=0; i<l; i++) {
+                    index = types.indexOf(words[i].toLocaleLowerCase());
+                    if (index > -1) {
+                        type_inp.value = index;
+                        break;
+                        title_inp.onBlur = null;
+                    }
+                }
+                */
+            };
+        }
 
     addInp();
 });
