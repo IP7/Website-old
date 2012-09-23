000cf068a6a46eeef5999e228154ca2f84d8a46d
Baptiste Fontaine <batifon@yahoo.fr>
2012-09-23 23:30:05 +0200

    fix #182.

--- views/static/scripts/connected_home.js
+++ views/static/scripts/connected_home.js
@@ -1,9 +1,7 @@
 $(function(){
     (function display_last_contents() {
 
-        var div    = document.getElementById('last_contents'),
-            loader = document.createElement('image'),
-            t;
+        var div    = document.getElementById('last_contents');
 
         if (!div) { return; }
 
@@ -12,42 +10,53 @@ $(function(){
                 e.innerText = 'Une erreur est survenue lors du téléchargement.';
                 div.replaceChild(e, loader);
                 window.setTimeout(function(){$(div).fadeOut(1000)},5000);
-        };
+        },
 
-        loader.src = '/views/static/loader.gif';
-        div.appendChild(loader);
+        loader = document.createElement('image'),
+        t = document.createElement('h2'),
+        ul = document.createElement('ul');
 
-        $.ajax('/api/1/contents/last.json', {
-            data: { l:10 },
-            error: display_error,
-            success: function(response) {
-                if (!response || !response.response || !response.response.length) {
-                    return display_error();
-                }
-                var ul       = document.createElement('ul'),
-                    contents = response.response,
-                    len      = contents.length,
-                    i        = 0,
-                    li, a, c;
-
-                for (; i < len; i++) {
-                    c = contents[i];
-                    li = document.createElement('li');
-                    li.innerHTML = '<a href="' + c.href + '">' + c.title + '</a>'
-                                +  ' (' + c.course + ', ' + c.cursus + '), le '
-                                +  '<date datetime="' + c.date + '">'
-                                +  c.date.replace(' ', ' à ') + '</date>';
-
-                    ul.appendChild(li); 
-                }
-
-                div.replaceChild(ul, loader);
-            }
-        });
-
-        t = document.createElement('h2');
+        loader.src = './views/static/loader.gif';
         t.innerText = 'Les derniers contenus ajoutés';
-        div.insertBefore(t, loader);
+
+        div.appendChild(t);
+
+        (function update_last_contents() {
+
+            if (ul.parentNode === div) { div.removeChild(ul); }
+            div.appendChild(loader);
+
+            $.ajax('./api/1/contents/last.json', {
+                data: { l:10 },
+                error: display_error,
+                success: function(response) {
+                    if (!response || !response.response || !response.response.length) {
+                        return display_error();
+                    }
+                    var contents = response.response,
+                        len      = contents.length,
+                        i        = 0,
+                        li, a, c;
+
+                    while (ul.hasChildNodes()) { ul.removeChild(ul.lastChild); }
+
+                    for (; i < len; i++) {
+                        c = contents[i];
+                        li = document.createElement('li');
+                        li.innerHTML = '<a href="' + c.href + '">' + c.title + '</a>'
+                                    +  ' (' + c.course + ', ' + c.cursus + '), le '
+                                    +  '<date datetime="' + c.date + '">'
+                                    +  c.date.replace(' ', ' à ') + '</date>';
+
+                        ul.appendChild(li); 
+                    }
+
+                    div.replaceChild(ul, loader);
+
+                    window.setTimeout(update_last_contents, 600000);
+                }
+            });
+        })();
 
     })();
 });
