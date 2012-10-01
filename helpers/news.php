f66e2a8a74efef83d1496e969dfd5bab81d8e614
Baptiste Fontaine <batifon@yahoo.fr>
2012-10-01 23:34:22 +0200

    get_news helper fixed for null courses/cursus

--- helpers/news.php
+++ helpers/news.php
@@ -5,14 +5,20 @@ function get_news($cursus=null, $course=null) {
 
     $user_rights = is_connected() ? user()->getRank() : 0;
 
-    return NewsQuery::create()
-                ->filterByCursus($cursus)
-                ->filterByCourse($course)
+    $q = NewsQuery::create()
                 ->where('Access_Rights <= ?', $user_rights, PDO::PARAM_INT)
                 ->limit(10)
-                ->orderByDate('asc')
-                ->find();
+                ->orderByDate('asc');
 
+    if ($cursus) {
+        $q = $q->filterByCursus($cursus);
+    }
+
+    if ($course) {
+        $q = $q->filterByCourse($course);
+    }
+
+    return $q->find();
 }
 
 ?>
