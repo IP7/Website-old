de023c25a7c1142eaa5f5647ce62a314b1173e93
Baptiste Fontaine <batifon@yahoo.fr>
2012-10-02 23:49:47 +0200

    'cancel' button added.

--- views/static/scripts/news.js
+++ views/static/scripts/news.js
@@ -1,29 +1,76 @@
 $(function() {
 
-    var dom_news = document.querySelectorAll('.news[data-id][data-can-edit="1"]');
+    var dom_news = document.querySelectorAll('.news[data-id][data-can-edit="1"]'),
+
+        news = {};
 
     if (!dom_news.length) { return; }
 
-    [].forEach.call(dom_news, function(li){
-        var be = document.createElement('span');
+    [].forEach.call(dom_news, function(li) {
+        var be = document.createElement('span'),
+            bs = document.createElement('span'),
+            bc = document.createElement('span');
+
         be.innerText = 'éditer';
         be.className = 'button news-edit-button';
+        
+        bs.innerText = 'enregistrer';
+        bs.className = 'button news-edit-button';
+
+        bc.innerText = 'annuler';
+        bc.className = 'button news-edit-button';
+
         li.appendChild(be);
+        li.dataset['edited'] = false;
+
+        news[li.dataset['id']] = {
+            buttons : {
+                edit   : be,
+                save   : bs,
+                cancel : bc
+            }
+        };
 
         be.onclick = function() {
-            if (li.dataset['edited'] || be.classList.contains('disabled')) {return}
-            edit(li, be);
+            if (li.dataset['edited'] === 'true') {return}
+            edit(li);
+        };
+
+        bs.onclick = function() {
+            if (!li.dataset['edited']) {return}
+            save(li);
+        };
+
+        bc.onclick = function() {
+            if (!li.dataset['edited']) {return}
+            cancel_edit(li);
         };
     });
 
+    function cancel_edit(li) {
+        var id = li.dataset['id'],
+            bt = news[id].buttons;
 
-    function edit(li, edit_button) {
-        edit_button.classList.add('disabled');
+        li.dataset['edited'] = false;
+
+        li.childNodes[0].removeAttribute('contenteditable'); 
+        li.childNodes[1].removeAttribute('contenteditable'); 
+
+        li.childNodes[0].innerText = news[id].old_title;
+        li.childNodes[1].innerHTML = news[id].old_text;
+
+        bt.edit.classList.remove('disabled');
+        li.replaceChild(bt.edit, bt.cancel);
+    }
+
+    function edit(li) {
+        var id = li.dataset['id'],
+            bt = news[id].buttons;
 
         // TODO add a loading image
 
-        $.ajax('/api/1/news/get_one.json', {
-            data: { id:li.dataset['id'] },
+        $.ajax('../api/1/news/get_one.json', {
+            data: { id: id },
             success: function(resp) {
                 if (!resp['response']) {
                     edit_button.classList.remove('disabled');
@@ -39,9 +86,14 @@ $(function() {
                 body.setAttribute('contenteditable', true);
 
                 title.innerText = resp.title;
-                body.innerText  = resp.text;
+                body.innerText  = resp.md_text;
 
-                // TODO add buttons for save/cancel
+                news[id].old_title = resp['title'];
+                news[id].old_text  = resp['text'];
+
+                li.replaceChild(bt.cancel, bt.edit);
+
+                // TODO add buttons for saving
 
             },
             error: function() {
