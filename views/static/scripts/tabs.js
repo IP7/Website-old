c78f41fad18d8e21aea4d1600db270f9301d2c9e
Baptiste Fontaine <batifon@yahoo.fr>
2012-10-03 21:44:18 +0200

    Using JS for tabs, fixes #200.

--- views/static/scripts/tabs.js
+++ views/static/scripts/tabs.js
@@ -1,15 +1,29 @@
 $(function(){
-    (function($ta,s,$w,e) {
-        // add a .selected class on the selected tab
-        $w.bind(e, function(h){
-            if(!(h=window.location.hash))return;
-            $ta.parent().removeClass(s);
-            $('.tab a[href='+h+']').parent().addClass(s);
-        });
 
-        // automatically open the first tab if no tab is selected
-        if(!window.location.hash&&$ta.length)window.location.hash=$ta.first().attr('href');
-        // trigger hashchange on pageload
-        $w.trigger(e);
-    })($('.tab a'), 'selected', $(window), 'hashchange');
+    var $tabs = $('.tab[data-for]'),
+        $tb_contents = $('.tab-content[data-type]'),
+
+        selected = 'selected',
+        displayed = 'displayed'
+
+        $old_tab = $tabs.first(),
+        $old_tb_content = $tb_contents.first();
+
+    
+    $tabs.click(function() {
+        var $this = $(this);
+
+        if ($this === $old_tab) return;
+
+        var type  = $this.data('for'),
+            $ct   = $tb_contents.filter('[data-type="'+type+'"]');
+        
+        $old_tab.removeClass(selected);
+        $old_tb_content.removeClass(displayed);
+
+        $old_tab = $this.addClass(selected);
+        $old_tb_content = $ct.addClass(displayed);
+    });
+
+    $tabs.first().click();
 });
