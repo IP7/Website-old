94f8b6f8f0e6a002356f5401e483c66e10f6881f
Baptiste Fontaine <batifon@yahoo.fr>
2012-10-03 22:22:51 +0200

    indentation + somme cleaning

--- index.php
+++ index.php
@@ -42,85 +42,83 @@ function before($route) {
 }
 
 ## (get/post) home
-dispatch('/', 'display_home');
+dispatch('/',      'display_home');
 dispatch_post('/', 'display_home');
 
 ## (get) connection page
-dispatch('/connexion', 'display_connection');
+dispatch('/connexion',      'display_connection');
 ## (post) connection
 dispatch_post('/connexion', 'post_connection');
 
 ## forgotten password
-dispatch('/oubli', 'display_forgotten_password');
+dispatch('/oubli',      'display_forgotten_password');
 dispatch_post('/oubli', 'post_forgotten_password');
 
 ## users' profiles
-dispatch('/p/*', 'display_profile_page');
-dispatch('/p/*/edit', 'display_edit_profile_page');
+dispatch('/p/*',           'display_profile_page');
+dispatch('/p/*/edit',      'display_edit_profile_page');
 dispatch_post('/p/*/edit', 'post_edit_profile_page');
 ## my profile
-dispatch('/profile', 'display_my_profile_page');
+dispatch('/profile',      'display_my_profile_page');
 dispatch_post('/profile', 'display_my_profile_page');
 ## edit my profile
-dispatch('/profile/edit', 'display_edit_my_profile_page');
+dispatch('/profile/edit',      'display_edit_my_profile_page');
 dispatch_post('/profile/edit', 'post_edit_my_profile_page');
-dispatch('/profile/init', 'display_init_my_profile_page');
+dispatch('/profile/init',      'display_init_my_profile_page');
 dispatch_post('/profile/init', 'post_init_my_profile_page');
 # 
 # ## search
 dispatch('/recherche', 'display_search_results');
 
 ## cursus
-dispatch('/cursus/:name', 'display_cursus');
+dispatch('/cursus/:name',      'display_cursus');
 dispatch_post('/cursus/:name', 'display_cursus');
 dispatch('/cursus/:name/edit', 'display_moderation_edit_cursus');
 ## educational paths
 dispatch('/cursus/:cursus/parcours/:path', 'display_educational_path');
-## - news
-#dispatch('/cursus/:name/add_news',      'display_add_cursus_news');
-#dispatch_post('/cursus/:name/add_news', 'display_add_cursus_news');
 
 ## course
 dispatch('/cursus/:cursus/:course', 'display_course');
 ## contents
-dispatch('/cursus/:cursus/:course/proposer', 'display_member_proposing_content_form');
+dispatch('/cursus/:cursus/:course/proposer',      'display_member_proposing_content_form');
 dispatch_post('/cursus/:cursus/:course/proposer', 'display_post_member_proposed_content');
-dispatch_post('/cursus/:cursus/:course/proposer/prévisualiser', 'display_post_member_proposed_content_preview');
-dispatch('/cursus/:cursus/:course/:id', 'display_course_content');
+dispatch_post('/cursus/:cursus/:course/proposer/prévisualiser',
+                                                  'display_post_member_proposed_content_preview');
+dispatch('/cursus/:cursus/:course/:id',           'display_course_content');
 
 ## contents' files
-dispatch('/file/:id', 'serve_user_file');
+dispatch('/file/:id',       'serve_user_file');
 dispatch('/file/:id/:name', 'serve_user_file_by_id_and_name');
 
 ## admin home
 dispatch('/admin', 'display_admin_home');
 ## moderation 
-dispatch('/admin/moderation', 'display_admin_moderation');
-dispatch('/admin/reports', 'display_admin_content_report');
-dispatch_post('/admin/reports', 'post_admin_content_report');
-dispatch('/admin/content/proposed', 'display_admin_proposed_content');
+dispatch('/admin/moderation',            'display_admin_moderation');
+dispatch('/admin/reports',               'display_admin_content_report');
+dispatch_post('/admin/reports',          'post_admin_content_report');
+dispatch('/admin/content/proposed',      'display_admin_proposed_content');
 dispatch_post('/admin/content/proposed', 'post_admin_proposed_content');
-dispatch('/admin/content/proposed/:id', 'display_admin_content_view');
+
 ## finances
-dispatch('/admin/membres', 'display_admin_members');
-dispatch('/admin/membres/add', 'display_admin_add_member');
+dispatch('/admin/membres',          'display_admin_members');
+dispatch('/admin/membres/add',      'display_admin_add_member');
 dispatch_post('/admin/membres/add', 'post_admin_add_member');
 ## maintenance
 dispatch('/admin/migrate', 'display_admin_migrate_db_page');
 
 ## (almost-)static pages
-dispatch('/contact', 'display_contact_page');
-dispatch('/sitemap', 'display_sitemap_page');
+dispatch('/contact',  'display_contact_page');
+dispatch('/sitemap',  'display_sitemap_page');
 dispatch('/legals',   'display_legals_page');
-dispatch('/bug', 'display_bug_report');
+dispatch('/bug',      'display_bug_report');
 dispatch_post('/bug', 'post_bug_report');
 dispatch('/a-propos', 'display_apropos_page');
 
 ## API
-dispatch('/api/1/users/exists.json',  'json_check_username');
-dispatch('/api/1/search.json',        'json_global_search');
-dispatch('/api/1/contents/last.json', 'json_get_last_contents');
-dispatch('/api/1/news/get_one.json',  'json_get_news_by_id');
+dispatch('/api/1/users/exists.json',     'json_check_username');
+dispatch('/api/1/search.json',           'json_global_search');
+dispatch('/api/1/contents/last.json',    'json_get_last_contents');
+dispatch('/api/1/news/get_one.json',     'json_get_news_by_id');
 dispatch_post('/api/1/news/update.json', 'json_post_update_news');
 
 ## tests
