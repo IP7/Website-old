0d4d9106fbf954ef904863aea468c236024bbd38
Baptiste Fontaine <batifon@yahoo.fr>
2013-01-19 18:48:00 +0100

    tabs' links added, fixes #248.

--- views/static/scripts/tabs.js
+++ views/static/scripts/tabs.js
@@ -5,25 +5,42 @@ $(function(){
 
         selected = 'selected',
         displayed = 'displayed',
+        w = window,
 
-        $old_tab = $tabs.first(),
-        $old_tb_content = $tb_contents.first();
+        hist = w.history && w.history.replaceState.bind(w.history, '') || $.noop,
 
-    
-    $tabs.click(function() {
-        var $this = $(this);
+        hash = w.location.hash.slice(1),
+
+        $old_tab = $tabs.first(), tmp;
+
+    $tabs.each(function(i, e) {
+
+        $(e).data( 'ct', $tb_contents.eq(i) )
+            .data( 'title', e.innerText.toLocaleUpperCase() );
+
+    }).click(function() {
+        var $this = $(this),
+            hash  = $this.data('for'),
+            $ct   = $this.data('ct');
 
         if ($this === $old_tab) return;
 
-        var type  = $this.data('for'),
-            $ct   = $tb_contents.filter('[data-type="'+type+'"]');
-        
+        hist($this.data('title'), '#' + hash);
+
         $old_tab.removeClass(selected);
-        $old_tb_content.removeClass(displayed);
+        $old_tab.data('ct').removeClass(displayed);
 
         $old_tab = $this.addClass(selected);
-        $old_tb_content = $ct.addClass(displayed);
+        $ct.addClass(displayed);
     });
 
-    $tabs.first().click();
+    if ( hash != '' && (tmp = $tabs.filter('[data-for='+hash+']').first()).length === 1 ) {
+
+        $old_tab = tmp;
+
+    }
+
+    $old_tb_content = $old_tab.data('ct');
+    $old_tab.click();
+
 });
