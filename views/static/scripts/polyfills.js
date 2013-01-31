2bde0c7120fb1d69b478e534fc8cac462f294b8b
Baptiste Fontaine <batifon@yahoo.fr>
2013-02-01 00:05:46 +0100

    Using ES5-Shim for ES5 Polyfills

--- views/static/scripts/polyfills.js
+++ views/static/scripts/polyfills.js
@@ -1,17 +1,3 @@
-// For iOS <= 4
-Function.prototype.bind = Function.prototype.bind || function (){
-    var that = this,
-        args = [].slice.apply(arguments),
-        context = args.shift();
-
-    return function(){
-        return that.apply(
-            context,
-            args.concat([].slice.apply(arguments))
-        );
-    };
-};
-
 // For IE <= 8
 if (!Object.prototype.hasOwnProperty.call(HTMLElement.prototype, 'textContent')) {
     HTMLElement.prototype.__defineGetter__('textContent', function() {
