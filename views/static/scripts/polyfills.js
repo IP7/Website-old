09f359a5989acaddc95a8df8fd49ad1049a954b9
Baptiste Fontaine <batifon@yahoo.fr>
2013-03-12 18:32:31 +0100

    Object.create polyfill added.

--- views/static/scripts/polyfills.js
+++ views/static/scripts/polyfills.js
@@ -7,3 +7,15 @@ if (!Object.prototype.hasOwnProperty.call(HTMLElement.prototype, 'textContent'))
         this.innerText = e;
     });
 }
+
+// For Firefox <4 & old browsers
+if ( typeof Object.create === 'undefined' ) {
+
+    // From Douglas Crockford's "JavaScript: The Good Parts"
+    Object.create = function (o) { 
+        function F() {} 
+        F.prototype = o; 
+        return new F(); 
+    };
+
+}
