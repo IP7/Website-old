e61898898b1dee7641508f6a99aae7a53503ac5a
Baptiste Fontaine <batifon@yahoo.fr>
2013-04-26 17:59:13 +0200

    Disqus code removed.

--- views/static/scripts/disqus.js
+++ views/static/scripts/disqus.js
@@ -1,8 +1,9 @@
 // disqus config has to be global :(
+/*
 var disqus_shortname = 'ip7';
 
 (function() {
     var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
     dsq.src = 'http://ip7.disqus.com/embed.js';
     document.getElementsByTagName('head')[0].appendChild(dsq);
-})();
+})();*/
