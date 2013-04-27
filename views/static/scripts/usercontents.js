1eb355354e74dff246239f66b82e04f86a5a76f0
Baptiste Fontaine <batifon@yahoo.fr>
2013-04-27 17:19:28 +0200

    Don't try to edit a field if $.fn.editable is not available.

--- views/static/scripts/usercontents.js
+++ views/static/scripts/usercontents.js
@@ -4,10 +4,12 @@ $(function() {
         prettyPrint();
     }
 
-    $( '.content_header h1' ).first()
-        .attr( 'id', $( '.user_content' ).first().data( 'contentId' ) )
-        .editable( '/api/1/edit/content/title.html', {
-            indicator : 'sauvegarde…',
-            tooltip : 'Cliquez pour éditer'
-        });
+    if ($.fn.editable) {
+        $( '.content_header h1' ).first()
+            .attr( 'id', $( '.user_content' ).first().data( 'contentId' ) )
+            .editable( '/api/1/edit/content/title.html', {
+                indicator : 'sauvegarde…',
+                tooltip : 'Cliquez pour éditer'
+            });
+    }
 });
