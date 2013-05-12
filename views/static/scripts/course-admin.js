f67754794e8e46ec248567935f4896e7ac5ed6dd
Baptiste Fontaine <batifon@yahoo.fr>
2013-05-12 21:21:17 +0200

    Admins now have to double-click on courses' intros to edit them (fixes #326)

--- views/static/scripts/course-admin.js
+++ views/static/scripts/course-admin.js
@@ -1,16 +1,17 @@
 $(function() {
 
-    var $intro = $( '.course-infos .intro' )
-                    .attr( 'id', $( 'article.course' ).data( 'courseId' ) )
-                    .editable('/api/1/edit/course/intro.html', {
-                        loadurl : '/api/1/edit/course/intro.md',
-                        type : 'textarea',
-                        submit : 'OK',
-                        cols : 80,
-                        rows : 8,
-                        placeholder : '(pas de description)',
-                        indicator : 'sauvegarde…',
-                        tooltip : 'Cliquez pour éditer'
-                    });
+    $( '.course-infos .intro' )
+       .attr( 'id', $( 'article.course' ).data( 'courseId' ) )
+       .editable('/api/1/edit/course/intro.html', {
+           loadurl : '/api/1/edit/course/intro.md',
+           type : 'textarea',
+           submit : 'OK',
+           cols : 80,
+           rows : 8,
+           placeholder : '(pas de description)',
+           indicator : 'sauvegarde…',
+           tooltip : 'Cliquez pour éditer',
+           event : 'dblclick'
+    });
 
 });
