a602d252a021f18e98588bd9d754a02e12549ad7
Baptiste Fontaine <batifon@yahoo.fr>
2013-06-08 17:47:54 +0200

    Page load code removed, it's now natively integrated into Piwik.

--- views/static/scripts/piwik.js
+++ views/static/scripts/piwik.js
@@ -5,24 +5,7 @@
 
     $.getScript( b + 'piwik.js', function() {
 
-        var piwikTracker = Piwik.getTracker(b + 'piwik.php', 1),
-            p, navigation_time, page_load;
-
-        // perfs
-        if (   ( typeof ( p = window.performance ) === 'object' )
-            && ( typeof (p = p.timing ) === 'object' ) ) {
-
-            navigation_time = p.loadEventEnd - p.navigationStart;
-            page_load       = p.loadEventEnd - p.responseEnd;
-
-            if ( navigation_time > 0 || page_load > 0 ) {
-
-                piwikTracker.setCustomVariable( 1, 'nt', '' + navigation_time, 'page' );
-                piwikTracker.setCustomVariable( 2, 'pl', '' + page_load,       'page' );
-
-            }
-
-        }
+        var piwikTracker = Piwik.getTracker(b + 'piwik.php', 1);
 
         piwikTracker.trackPageView();
         piwikTracker.enableLinkTracking();
