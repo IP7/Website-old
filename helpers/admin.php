7f9aff78f9271688574e43b22407b3cfea5ba898
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-19 21:08:31 +0200

    unnecessary admin anchor link removed

--- helpers/admin.php
+++ helpers/admin.php
@@ -4,14 +4,11 @@ function admin_tpl_default() {
 
     return array(
         'page' => array(
-
             'breadcrumbs' => array(
                 array( 'title' => 'Administration', 'href' => Config::$root_uri.'admin' )
             ),
 
             'navlinks' => array(
-                array('title' => 'Modération',  'href' => Config::$root_uri.'admin/#mod'),
-
                 array('title' => 'Retour au site', 'href' => Config::$root_uri)
             )
         )
